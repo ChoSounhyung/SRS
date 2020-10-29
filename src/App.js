c7587322410df6ae2962cf64a15d0a1fa3a9a047
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-30 00:16:53 +0900

    상점 리스트, 카드 게임 Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter, Route} from "react-router-dom";
+import Sangjum_List from "./routes/Sangjum_List";
+import Card_Game from "./routes/Card_Game";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Route path="/sangjum_list" exact={true} component={Sangjum_List} />
+      <Route path="/card_game" exact={true} component={Card_Game} />
+    </HashRouter>
   );
 }
 
---
+++ src/routes/Card_Game.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function  Card_Game() {
+    return <h1>Card Game page</h1>
+}
+
+export default Card_Game;
