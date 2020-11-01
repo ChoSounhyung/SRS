4b12257b12f420d5b285be51c721f89cd10f9072
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-02 01:20:57 +0900

    Navigation에 inner text 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,10 +7,16 @@ function Navigation() {
     return (
         <div className="parent">
             <div className="box" id="s_list">
-                <Link className="hiper" to="/sangjum_list">상점 리스트</Link>
+                <Link className="hiper" to="/sangjum_list">
+                    상점 리스트
+                    <div className="inner_text">Sanjum List</div>
+                </Link>
             </div>
             <div className="box" id="c_game">
-                <Link className="hiper" to="/card_game">카드 게임</Link>
+                <Link className="hiper" to="/card_game">
+                    카드 게임
+                    <div className="inner_text">Card Game</div>
+                </Link>
             </div>
         </div>
     );
