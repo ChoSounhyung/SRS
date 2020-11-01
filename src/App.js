3c406a50b5b1421ac232ddd56b92816adbae6294
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-01 23:59:38 +0900

    Navigation css file import & 적용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { HashRouter, Route} from "react-router-dom";
 import Sangjum_List from "./routes/Sangjum_List";
 import Card_Game from "./routes/Card_Game";
-import Navigation from './components/Navigation';
-
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (
+  return  (
     <HashRouter>
       <Navigation />
       <Route path="/sangjum_list" exact={true} component={Sangjum_List} />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
+import "./Navigation.css"
+
 function Navigation() {
     return (
         <div>
-            <Link to="/sangjum_list">Sangjum_List</Link>
-            <Link to="/card_game">Card_Game</Link>
+            <div className="box">
+                <Link className="hiper" to="/sangjum_list">상점 리스트</Link>
+            </div>
+            <div className="box">
+                <Link className="hiper" to="/card_game">카드 게임</Link>
+            </div>
         </div>
     );
 }
