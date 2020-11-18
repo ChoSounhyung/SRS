ad76489e3690aefbd4190ea5199066715dc144a5
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-18 23:17:30 +0900

    id 추가, json 파일 읽어옴

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,8 @@ import {Link} from "react-router-dom";
 
 import "./Navigation.css"
 
-const nav_menu = ["LIST", "SMTI"]
-
 class Navigation extends React.Component {
     render() {
-        const textDecoration = {
-            textDecoration: 'underline #3CB432',
-        };
-    
         return (
             <div className="nav">
                 <Link to="/list" className="nav_text" id="lsit">LIST</Link>
--- src/routes/List.js
+++ src/routes/List.js
@@ -1,8 +1,24 @@
 import React from "react";
+import storeData from "../StoreData.json"
+
+function Store({ name, address }) {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <h3>{address}</h3>
+        </div>
+    );
+}
 
 class List extends React.Component {
     render() {
-        return <h1>List page</h1>
+        return (
+            <div>
+                {storeData.map(store => (
+                    <Store key={store.id} name={store.name} address={store.address} />
+                ))}
+            </div>
+        );
     }
 }
 export default List;
