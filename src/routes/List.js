0335d2a35418fdd70d33c2d4fa58b6ac8ccdf011
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-19 23:46:38 +0900

    list media query

--- src/routes/List.js
+++ src/routes/List.js
@@ -4,7 +4,7 @@ import storeData from "../StoreData.json"
 
 function Store({ name, address, image }) {
     return (
-        <div className="green_box">
+        <div className="green_box" id="green_box_container">
             <img className="crop" src={image} alt={name} />
             <div className="content">
                 <h3 id="name">{name}</h3>
@@ -17,7 +17,7 @@ function Store({ name, address, image }) {
 class List extends React.Component {
     render() {
         return (
-            <div className={"list_container"}>
+            <div className={"list_container"} id="container_margin">
                 {storeData.map(store => (
                     <Store key={store.id} name={store.name} address={store.address} image={store.image} />
                 ))}
