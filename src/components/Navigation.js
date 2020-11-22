d92bdda75a464cc1feb3dcc3d7f752b401fb7139
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-23 01:13:20 +0900

    add srs image & connect link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 import "./Navigation.css"
+import srs from "../image/srs_icon.png"
 
 class Navigation extends React.Component {
     render() {
         return (
-            <div className="nav">
-                <Link to="/list" className="nav_text" id="lsit">LIST</Link>
-                <Link to="/smti" className="nav_text" id="smti">SMTI</Link>
+            <div className="nav_container">
+                <Link to="/#"><img className="srs" src={srs}/></Link>
+                <div className="menu_container">
+                    <Link to="/list" className="nav_text" id="menu">LIST</Link>
+                    <Link to="/smti" className="nav_text" id="menu">SMTI</Link>
+                </div>
             </div>
         );
     }
