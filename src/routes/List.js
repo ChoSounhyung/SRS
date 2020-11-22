b2f966e22962d1b203c626168b09dbe63b81b917
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-22 20:41:58 +0900

    onclick event

--- src/routes/List.js
+++ src/routes/List.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./List.css"
 import storeData from "../StoreData.json"
 
-function Store({ name, address, image }) {
+function Store({ name, address, image, link }) {
     return (
+        <a href={link} onClick="console.log('clicked') return false">
         <div className="green_box" id="green_box_container">
             <img className="crop" src={image} alt={name} />
             <div className="content">
@@ -11,6 +12,7 @@ function Store({ name, address, image }) {
                 <h3 id="address">{address}</h3>
             </div>
         </div>
+        </a>
     );
 }
 
@@ -21,7 +23,7 @@ class List extends React.Component {
                 <h1 className="title">신사리 상점 리스트</h1>
                 <div className={"list_container"} id="container_margin">
                     {storeData.map(store => (
-                        <Store key={store.id} name={store.name} address={store.address} image={store.image} />
+                        <Store key={store.id} name={store.name} address={store.address} image={store.image} link={store.link} />
                     ))}
                 </div>
             </div>
