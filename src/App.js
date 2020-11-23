b1919ca9586f3ae86f30c68e4c94f13d7e91abc8
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-23 22:45:38 +0900

    main page text

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { HashRouter, Route} from "react-router-dom";
 import Smti from "./routes/Smti";
 import List from "./routes/List";
 import Navigation from "./components/Navigation";
+import "./App.css"
 
 class App extends React.Component {
   render() {
     return (
-      <HashRouter>
+      <div>
+        <HashRouter>
         <Navigation />
         <Route path="/list" exact={true} component={List} />
         <Route path="/smti" exact={true} component={Smti} />
       </HashRouter>
+      <p><small>This stop is </small>Sinsa-ri Station</p>
+      </div>
     );
   }
 }
