42d4c72603fccd31e87bb402c41b0424f414e684
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-24 14:29:29 +0900

    home text modify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home"
 import Smti from "./routes/Smti";
 import List from "./routes/List";
 import Navigation from "./components/Navigation";
-import "./Home.css"
+import "./App.css"
 
-class Home extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
         <HashRouter>
           <Navigation />
-          <Route path="/list" exact={true} component={List} />
-          <Route path="/smti" exact={true} component={Smti} />
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/list" component={List} />
+          <Route path="/smti" component={Smti} />
         </HashRouter>
       </div>
     );
   }
 }
 
-export default Home;
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css"
+
+class Home extends React.Component {
+    render() {
+        return (
+            <p><small>This stop is </small>Sinsa-ri Station</p>
+        );
+    }
+}
+
+export default Home;
