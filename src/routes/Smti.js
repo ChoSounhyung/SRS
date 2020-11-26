376580da234933c8ccf2c4fcf20b216e6cc87a03
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-27 03:52:07 +0900

    Json Data로 셋팅, Progress Bar

--- src/routes/Smti.js
+++ src/routes/Smti.js
@@ -1,23 +1,45 @@
 import React from "react";
 import "./Smti.css";
 
+// smti data
+import smti_data from "../data/smti_data.json";
+
 class Smti extends React.Component {
+    state = {
+        datas: smti_data,
+        currentIndex: 0
+    }
+
+    clickHandle = (evt) => {
+        const result = evt.target.innerText;
+        console.log(result);
+
+        const { currentIndex, datas } = this.state;
+        if (currentIndex != datas.length - 1) {
+            this.setState({
+                currentIndex: currentIndex + 1
+            })
+        } else {
+            // Redirect
+        }
+    }
+
     render() {
+        const { datas, currentIndex } = this.state;
         return <div id="smti_container">
             <div className="quiz_container">
-                <h1>엽떡 맵기를 고를 때 나는</h1>
+                <h1>{datas[currentIndex].text}</h1>
                 <div className="select_container">
-                    <div className={"select_box"}>
-                        맵찔이지만 허세부리며 중간맛을 고른다
-                    </div>
-                    <div className={"select_box"}>
-                        맵찔이지만 허세부리며 중간맛을 고른다
-                    </div>
+                    {datas[currentIndex].examples.map((example) => (
+                        <div className={"select_box"} onClick={this.clickHandle}>
+                            {example}
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="progress_bar">
-                {[1, 2, 3, 4, 5, 6].map((n) => 
-                    <div className="circle" />
+                {datas.map((data, index) => 
+                    <div className={index == currentIndex ? "circle active" : "circle"} />
                 )}
             </div>
         </div>
