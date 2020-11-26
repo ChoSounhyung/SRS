25ec1722fb594eec6b7ebd87b60aeb2e7c866a16
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-27 03:40:30 +0900

    SMTI Page Design

--- src/routes/Smti.js
+++ src/routes/Smti.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Smti.css";
 
 class Smti extends React.Component {
     render() {
         return <div id="smti_container">
-
+            <div className="quiz_container">
+                <h1>엽떡 맵기를 고를 때 나는</h1>
+                <div className="select_container">
+                    <div className={"select_box"}>
+                        맵찔이지만 허세부리며 중간맛을 고른다
+                    </div>
+                    <div className={"select_box"}>
+                        맵찔이지만 허세부리며 중간맛을 고른다
+                    </div>
+                </div>
+            </div>
+            <div className="progress_bar">
+                {[1, 2, 3, 4, 5, 6].map((n) => 
+                    <div className="circle" />
+                )}
+            </div>
         </div>
     }
 }
