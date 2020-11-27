28b9e329101831c172f9b6536e91129331570014
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-27 12:15:55 +0900

    result json, import image

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Result.css";
 
 // image
+import skate from "../image/result/1.png"
 import ramen from "../image/result/2.png";
+import meat from "../image/result/3.png"
+import chicken from "../image/result/4.png"
+import coffee from "../image/result/5.png"
+import rice from "../image/result/6.png"
+import stew from "../image/result/7.png"
+import tteokgalbi from "../image/result/8.png"
+import sundae from "../image/result/9.png"
+import shu from "../image/result/10.png"
 
 class Result extends React.Component {
     render() {
