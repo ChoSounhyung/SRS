77cebbad176c079766054e34c20c9d9fc26cb067
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-27 16:18:58 +0900

    Result Page Json 기반

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Result.css";
 
+import resultData from "../data/result_data.json"
+
 // image
 import skate from "../image/result/1.png"
 import ramen from "../image/result/2.png";
@@ -14,17 +16,33 @@ import sundae from "../image/result/9.png"
 import shu from "../image/result/10.png"
 
 class Result extends React.Component {
+    state = {
+        images: [
+            skate,
+            ramen,
+            meat, 
+            chicken, 
+            coffee, 
+            rice, 
+            stew, 
+            tteokgalbi, 
+            sundae, 
+            shu
+        ],
+        id: this.props.match.params.id,
+        data: resultData
+    }
+
     render() {
+        const { images, id, data } = this.state;
         return (
             <div id="result_container">
                 <div className="card_box">
-                    <span className="desc_type">볼 수록 빠져드는 매력</span>
-                    <h1 className="text_type">컵라면</h1>
-                    <img src={ramen} className="img_type" />
+                    <span className="desc_type">{data[id].title}</span>
+                    <h1 className="text_type">{data[id].name}</h1>
+                    <img src={images[id]} className="img_type" />
                     <div className="content_type">
-                        {["공감능력이 좋아서 상담을 잘 해줘요. 내 주변 사람이라면 내가 챙겨줄줄 알아야 된다고 생각해요.",
-                        "남들 얼굴이나 이름도 잘 외우고 신경써줘요.",
-                        "배려를 잘 해주고 감성적인 면이 발달해 있다면 현실적인 면도 발달해 있어서 계획을 잘 세우고 잘 지켜요."].map(text => (
+                        {data[id].contents.map(text => (
                             <span className="text_content">{text}</span>
                         ))}
                     </div>
