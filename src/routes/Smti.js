f61baac6bbf75618b09fe3d1598415fae4eaae16
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-27 16:12:11 +0900

    선택에 따라 캐릭터 아이디 얻음

--- src/routes/Smti.js
+++ src/routes/Smti.js
@@ -11,30 +11,63 @@ class Smti extends React.Component {
         checkList: {}
     }
 
+    componentDidMount() {
+        let checkListTemp;
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 2; j++) {
+                checkListTemp = {
+                    ...checkListTemp,
+                    [`${i}-${j}`]: 0
+                }
+            }
+        }
+        this.setState({
+            checkList: checkListTemp
+        })
+    }
+
     clickHandle = (evt) => {
         const result = evt.target.innerText;
         this.setResult(result);
 
-        const { currentIndex, datas } = this.state;
+        const { currentIndex, datas, checkList } = this.state;
         if (currentIndex != datas.length - 1) {
             this.setState({
                 currentIndex: currentIndex + 1
             })
         } else {
+            const types = [
+                checkList["3-1"] + checkList["5-1"] + checkList["7-0"],
+                checkList["2-1"] + checkList["3-1"] + checkList["5-1"] + checkList["6-1"] + checkList["7-1"],
+                checkList["0-1"] + checkList["2-0"] + checkList["4-1"] + checkList["8-1"],
+                checkList["2-0"] + checkList["3-1"] + checkList["5-1"] + checkList["8-0"],
+                checkList["0-1"] + checkList["1-0"] + checkList["3-0"] + checkList["6-0"] + checkList["7-1"],
+                checkList["1-1"] + checkList["5-1"] + checkList["6-1"] + checkList["8-0"] + checkList["8-1"],
+                checkList["0-0"] + checkList["1-1"] + checkList["2-1"] + checkList["3-1"] + checkList["4-1"],
+                checkList["0-1"] + checkList["2-0"] + checkList["3-0"] + checkList["7-1"] + checkList["8-1"],
+                checkList["1-0"] + checkList["3-0"] + checkList["4-0"] + checkList["5-0"] + checkList["7-0"],
+                checkList["0-1"] + checkList["1-1"] + checkList["4-0"] + checkList["5-1"] + checkList["6-1"],
+            ]
+            let id;
+            const maxValue = Math.max(...types);
+            for (let i = 0; i < types.length; i++) {
+                const num = types[i];
+                if (maxValue == num) {
+                    id = i;
+                    break;
+                }
+            }
             // Redirect
-            const id = 1;
             const { history } = this.props;
             history.push(`/result/${id}`);
         }
-        console.log(this.state.checkList);
     }
 
     setResult = (result) => {
         const { datas, checkList, currentIndex } = this.state;
-
         datas[currentIndex].examples.map((example, index) => {
             if (result === example) {
-                const count = checkList[`${currentIndex}-${index}`] || 0;
+                const count = checkList[`${currentIndex}-${index}`];
                 this.setState({
                     checkList: {
                         ...checkList,
